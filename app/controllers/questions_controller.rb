31172c2c95967445987e31ff0b3439122f899425
Stephanie Opdam <scm.opdam@gmail.com>
2020-01-14 20:29:20 +0000

    Stupid Coaching (excl. optional)

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @input = params[:question]
+    if @input == "I am going to work"
+      @answer = "Great!"
+    elsif @input.include? '?'
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
